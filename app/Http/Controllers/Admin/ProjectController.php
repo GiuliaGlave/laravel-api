8e7859a1448eed28d2d73074f9c939139f6ef403
GiuliaGlave <giulia.glave@gmail.com>
2023-04-19 17:16:43 +0200

    merge form create + edit

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -34,7 +34,8 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $project = new Project;
+        return view('admin.projects.form', compact('project'));
     }
 
     /**
@@ -74,7 +75,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        return view('admin.projects.edit', compact('project'));
+        return view('admin.projects.form', compact('project'));
     }
 
     /**
