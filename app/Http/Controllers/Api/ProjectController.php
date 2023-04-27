759f097da39b8ed82011784f00f760ef0fbe1dc5
GiuliaGlave <giulia.glave@gmail.com>
2023-04-27 18:37:31 +0200

    fixed show + slug

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -44,14 +44,15 @@ class ProjectController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show($slug)
     {
-        $project = Project::find($id);
+        $project = Project::where ('slug', $slug)
+        ->with('technologies', 'type')->first();
         if(!$project) return response(null, 404);
         
-         return response()->json($projects);
+         return response()->json($project);
     }
-    }
+    
 
     /**
      * Update the specified resource in storage.
